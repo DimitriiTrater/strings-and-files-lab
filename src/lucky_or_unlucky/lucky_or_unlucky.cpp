ae589805e85799c0368ee1bf0e7d2141e855f9af
DimitriiTrater <dima1603040@gmail.com>
2022-11-29 21:24:20 +0200

    Complete first task

--- src/lucky_or_unlucky/lucky_or_unlucky.cpp
+++ src/lucky_or_unlucky/lucky_or_unlucky.cpp
@@ -4,16 +4,34 @@
 std::string setTicket()
 {
     std::string a;
+    int num = 0;
+    
+    std::cout << "Enter the ticket number ";
     std::cin >> a;
+
+    if (a.size() != 6)
+        throw "Invalid ticket";
+    
     return a;
 }
 
-
+void checkAllElems(std::string str)
+{
+    for (int i = 0; i < str.size(); i++)
+    {
+        char element = str[i];
+        int temp = element - '0';
+        if (temp < 10)
+            throw "Invalid ticket element";
+    }
+}
 
 bool ticketCheck()
 {
     std::string ticket = setTicket();
     
+    checkAllElems(ticket);
+
     int sum_one = 0;
     int sum_two = 0;
     
@@ -46,6 +64,6 @@ void printResultTicketCheck()
         std::cout << "Lucky ticket!" << std::endl;
         return;
     }
-    
+
     std::cout << "Unlucky ticket =(" << std::endl;
 }
