5b1a335144be8d6da86b9d63ea7c8b1dbba05601
DimitriiTrater <dima1603040@gmail.com>
2022-12-01 20:16:32 +0200

    Complete to roman

--- src/from_arab_to_roman/from_arab_to_roman.cpp
+++ src/from_arab_to_roman/from_arab_to_roman.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <map>
 
 
 std::string getRoman(int number)
 {
-    return "result";
+    std::map<int, std::string> m = 
+    {
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"}, 
+        {40, "XL"}, 
+        {10, "X"}, 
+        {9, "IX"}, 
+        {5, "V"}, 
+        {4, "IV"}, 
+        {1, "I"},
+    };
+
+    int levels[] = 
+    {
+        1000, 900, 
+        500, 400, 
+        100, 90, 
+        50, 40, 
+        10, 9, 
+        5, 4, 
+        1,
+    };
+    std::string result = " ";
+
+    for (const int &level : levels)
+    {
+        if (number >= level)
+        {
+            do 
+            {
+                result += m[level];
+                number -= level;
+                
+            } while (number >= level);
+        }
+    }
+    return result;
 }
